14227103ef1d4d1c341fc9b18821466437c245fb
likey4978@yahoo.com.tw <likey4978@yahoo.com.tw>
2021-05-28 17:47:41 +0800

    fix main.cpp to call test.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-using namespce std;
+#include "test.h"
+using namespace std;
 int main(){
     cout<<"test"<<endl;
+    cout<<MAX_VALUE<<endl;
+    people a = people("Marry");
+    cout<<a.get_name()<<endl;
     return 1;
 }
